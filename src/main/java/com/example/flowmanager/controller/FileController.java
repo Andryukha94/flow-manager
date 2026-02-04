f8512cc16d05270db593f6adbaeff242eef0ac28
AndreyJava <bounty8hunters@gmail.com>
2026-02-04 20:37:40 +0300

    init

--- src/main/java/com/example/flowmanager/controller/FileController.java
+++ src/main/java/com/example/flowmanager/controller/FileController.java
@@ -2,14 +2,8 @@ package com.example.flowmanager.controller;
 
 import com.example.flowmanager.dto.response.FileStatusResponse;
 import com.example.flowmanager.dto.response.UploadResponse;
-import com.example.flowmanager.entity.FileEntity;
-import com.example.flowmanager.mapper.FileMapper;
-import com.example.flowmanager.service.command.FileCommandService;
-import com.example.flowmanager.service.query.FileQueryService;
-import com.example.flowmanager.service.validation.FileUploadValidator;
-import com.example.flowmanager.util.FileUtils;
+import com.example.flowmanager.service.api.FileApiService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,40 +14,20 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/files")
 public class FileController {
 
-    private final FileCommandService commandService;
-    private final FileQueryService queryService;
-    private final FileMapper mapper;
-    private final FileUploadValidator validator;
+    private final FileApiService fileApiService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public UploadResponse upload(@RequestPart("file") MultipartFile file) {
-
-        validator.validate(file);
-
-        byte[] bytes = FileUtils.toBytes(file);
-        String contentType = FileUtils.contentTypeOrDefault(file);
-
-        FileEntity entity = commandService.uploadAndSend(
-                bytes,
-                file.getOriginalFilename(),
-                contentType
-        );
-
-        return mapper.toUploadResponse(entity);
+        return fileApiService.upload(file);
     }
 
     @GetMapping("/{id}/status")
     public FileStatusResponse status(@PathVariable Long id) {
-        return mapper.toStatusResponse(queryService.getById(id));
+        return fileApiService.status(id);
     }
 
     @GetMapping("/{id}/download")
     public ResponseEntity<byte[]> download(@PathVariable Long id) {
-        byte[] pdf = queryService.getResultPdfBytes(id);
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"result.pdf\"")
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(pdf);
+        return fileApiService.download(id);
     }
 }
